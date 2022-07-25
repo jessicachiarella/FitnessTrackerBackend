cd7d0d550c2b937dc618dc46d46fb38e2e1d424c
Carlos <chnava@me.com>
2022-07-25 10:20:38 -0500

    set up drop tables and create tables

--- db/seedData.js
+++ db/seedData.js
@@ -3,12 +3,51 @@
 const client = require("./client")
 
 async function dropTables() {
-  console.log("Dropping All Tables...")
+  console.log("Dropping All Tables...");
+  await client.query(`
+  DROP TABLE IF EXISTS users;
+  DROP TABLE IF EXISTS activities;
+  DROP TABLE IF EXISTS routines;
+  DROP TABLE IF EXISTS routine_activities;
+  `);
+  console.log("finished dropping tables")
   // drop all tables, in the correct order
 }
 
 async function createTables() {
   console.log("Starting to build tables...")
+  await client.query(`
+  CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+  );
+  CREATE TABLE activities(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT NOT NULL,
+  );
+  CREATE TABLE routines(
+    id SERIAL PRIMARY KEY,
+    "creatorId" INTEGER	REFERENCES users(id),
+    "isPublic" BOOLEAN DEFAULT false,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    goal TEXT	NOT NULL,
+  );
+  CREATE TABLE routine_activities(
+    id SERIAL PRIMARY KEY,
+    "routineId" INTEGER	REFERENCES routines ( id ),
+    "activityId" INTEGER	REFERENCES activities ( id ),
+    duration INTEGER,
+    count	INTEGER,
+    UNIQUE ("routineId", "activityId"),
+  )
+  `)
+
+
+
+
+
   // create all tables, in the correct order
 }
 
