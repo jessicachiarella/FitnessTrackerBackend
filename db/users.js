46d32752693214159300017eea109658666c8bd8
jessicachiarella <jessica.chiarella@gmail.com>
2022-07-25 14:38:22 -0500

    created user functions, first two tests pass

--- db/users.js
+++ db/users.js
@@ -1,9 +1,17 @@
 const client = require("./client");
+const bcrypt = require('bcrypt');
+
+
+
+
 
 // database functions
 
 // user functions
 async function createUser({ username, password }) {
+  const SALT_COUNT = 10;
+  const hashedPassword = bcrypt.hash(password, SALT_COUNT)
+  try {
     const {
       rows: [user],
     } = await client.query(
@@ -11,24 +19,78 @@ async function createUser({ username, password }) {
     INSERT INTO users(username, password)
     VALUES ($1, $2)
     ON CONFLICT (username) DO NOTHING
-    RETURNING *;
+    RETURNING username;
     `,
-      [username, password]
+      [username, hashedPassword]
     );
     return user;
+  } catch (error){
+    console.error(error);
+  }
 }
 
 async function getUser({ username, password }) {
+  const user = await getUserByUsername(username);
+  const hashedPassword = user.password;
+  const passwordsMatch = await bcrypt.compare(password, hashedPassword);
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+        SELECT id, username
+        FROM users
+        WHERE username=${username}
+        `);
 
+    if(passwordsMatch) {
+      return user;
+    }
+    else {
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+  }
 
 
 }
 
 async function getUserById(userId) {
+try {
+  const{
+    rows: [user],
+  } = await client.query(`
+  SELECT id, username, password
+  FROM users
+  WHERE id=${userId}
+  `)
 
+  if (!user) {
+    return null;
+  }
+  return user;
+} catch (error) {
+  console.error(error)
+}
 }
 
-async function getUserByUsername(userName) {
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+    SELECT *
+    FROM users
+    WHERE username=$1;    
+    `,
+      [username]
+    );
+
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
 
 }
 
